79866ab789982f3dc62edfdd47b17f7b34ccf0a2
JSK <yaplistener@gmail.com>
2026-08-30 18:40:48 +0530

    Docker Updated

--- src/test/java/com/bloodbank/BloodBankApplicationTests.java
+++ src/test/java/com/bloodbank/BloodBankApplicationTests.java
@@ -2,11 +2,23 @@ package com.bloodbank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:mysql://localhost:3306/bloodbank?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC",
+    "spring.datasource.username=root",
+    "spring.datasource.password=JK@62006",
+    "spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.jpa.show-sql=false",
+    "logging.level.com.bloodbank=WARN"
+})
 class BloodBankApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+		System.out.println("✅ Application context loaded successfully with MySQL!");
+	}
+
 }
